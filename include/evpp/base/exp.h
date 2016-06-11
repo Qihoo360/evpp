97564af6f22b1a91e35440beaec8b9d38018871b
weizili.build9 <zieckey@gmail.com>
2016-06-11 10:15:11 +0800

    Delete useless classes : FdEvent,IPAddress. And move some codes from sockets.h to sys_sockets.h

--- include/evpp/base/exp.h
+++ include/evpp/base/exp.h
@@ -2,6 +2,8 @@
 
 #include "platform_config.h"
 #include "evppbase_export.h"
+#include "sys_addrinfo.h"
+#include "sys_sockets.h"
 
 #ifdef H_OS_WINDOWS
 //! Define import lib macro. Add it in ANY CPP file in target host module.
--- src/buffer.cc
+++ src/buffer.cc
@@ -20,7 +20,7 @@ namespace evpp {
         // when there is enough space in this buffer, don't read into extrabuf.
         // when extrabuf is used, we read 128k-1 bytes at most.
         const int iovcnt = (writable < sizeof extrabuf) ? 2 : 1;
-        const ssize_t n = readv(fd, vec, iovcnt);
+        const ssize_t n = ::readv(fd, vec, iovcnt);
         if (n < 0) {
             *savedErrno = errno;
         } else if (static_cast<size_t>(n) <= writable) {
