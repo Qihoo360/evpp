d9a153d06a9489849b68f10db72b3295c27f3b9d
weizili <zieckey@gmail.com>
2016-06-11 09:49:17 +0800

    Add pingpong_server. Add WSACleanup() when application exits on windows.

--- examples/pingpong/server/main.cc
+++ examples/pingpong/server/main.cc
@@ -15,13 +15,19 @@ void OnMessage(const evpp::TCPConnPtr& conn,
 }
 
 int main(int argc, char* argv[]) {
-    std::string port = "9099";
-    if (argc == 2) {
-        port = argv[1];
+    std::string addr = "0.0.0.0:9099";
+    int thread_num = 4;
+    if (argc != 1 && argc != 3) {
+        printf("Usage: %s <port> <thread-num>\n", argv[0]);
+        printf("  e.g: %s 9099 12\n", argv[0]);
+        return 0;
     }
-    std::string addr = std::string("0.0.0.0:") + port;
+    if (argc == 3) {
+        addr = std::string("0.0.0.0:") + argv[1];
+        thread_num = atoi(argv[2]);
+    }
     evpp::EventLoop loop;
-    evpp::TCPServer server(&loop, addr, "TCPEcho", 1);
+    evpp::TCPServer server(&loop, addr, "TCPPingPongServer", thread_num);
     server.SetMesageHandler(&OnMessage);
     server.Start();
     loop.Run();
