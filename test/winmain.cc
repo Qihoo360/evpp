2a7a13f2a428264eda4004ac25f7caca74757257
weizili.build9 <zieckey@gmail.com>
2016-06-22 22:14:57 +0800

    Rename app -> apps. Modify evpp/exp.h

--- test/winmain.cc
+++ test/winmain.cc
@@ -1,17 +1,7 @@
 #include "test_common.h"
 
+#include "evpp/exp.h"
 #include "evpp/libevent_headers.h"
-
-#ifdef WIN32
-#   ifdef _DEBUG
-#		pragma comment(lib, "libevpp.lib")
-#		pragma comment(lib,"libevent_d.lib")
-#   else
-#		pragma comment(lib, "libevpp.lib")
-#		pragma comment(lib,"libevent.lib")
-#   endif
-#	pragma comment(lib,"Ws2_32.lib")
-#endif
 
 namespace
 {
