203af93434107aa0e2d463a41026bd4528d45203
muyuwei <muyuwei@build19.kill.corp.qihoo.net>
2016-06-23 15:19:38 +0800

    add memcache client code into evmc directory

---
+++ apps/evmc/command.cc
@@ -0,0 +1,131 @@
+#include "command.h"
+
+#include <memcached/protocol_binary.h>
+#include "memcache_client.h"
+
+namespace evmc {
+
+void Command::Launch(MemcacheClientPtr memc_client) {
+    id_ = memc_client->next_id();
+    if (!memc_client->conn()) {
+        // TODO : 触发网络错误回调
+        LOG_ERROR << "Command bad memc_client " << memc_client << " id=" << id_;
+        return;
+    }
+
+    BufferPtr buf = RequestBuffer();
+    memc_client->conn()->Send(buf->data(), buf->size());
+}
+
+
+BufferPtr SetCommand::RequestBuffer() const {
+    protocol_binary_request_header req;
+    bzero((void*)&req, sizeof(req));
+
+    req.request.magic  = PROTOCOL_BINARY_REQ;
+    req.request.opcode = PROTOCOL_BINARY_CMD_SET;
+    req.request.keylen = htons(key_.size());
+    req.request.extlen = 8;
+    req.request.datatype = PROTOCOL_BINARY_RAW_BYTES;
+    req.request.vbucket  = vbucket_id();
+    req.request.opaque   = id();
+    size_t bodylen = req.request.extlen + key_.size() + value_.size();
+    req.request.bodylen = htonl(static_cast<uint32_t>(bodylen));
+
+    BufferPtr buf(new evpp::Buffer(sizeof(protocol_binary_request_header) + key_.size()));
+    buf->Append((void*)&req, sizeof(req));
+    buf->AppendInt32(flags_);
+    buf->AppendInt32(expire_);
+    buf->Append(key_.data(), key_.size());
+    buf->Append(value_.data(), value_.size());
+
+    return buf;
+}
+
+BufferPtr GetCommand::RequestBuffer() const {
+    protocol_binary_request_header req;
+    bzero((void*)&req, sizeof(req));
+
+    req.request.magic  = PROTOCOL_BINARY_REQ;
+    req.request.opcode = PROTOCOL_BINARY_CMD_GET;
+    req.request.keylen = htons(key_.size());
+    req.request.datatype = PROTOCOL_BINARY_RAW_BYTES;
+    req.request.vbucket  = vbucket_id();
+    req.request.opaque   = id();
+    req.request.bodylen = htonl(key_.size());
+
+    BufferPtr buf(new evpp::Buffer(sizeof(protocol_binary_request_header) + key_.size()));
+    buf->Append((void*)&req, sizeof(req));
+    buf->Append(key_.data(), key_.size());
+
+    return buf;
+}
+
+void MultiGetCommand::OnMultiGetCommandDone(int resp_code, const std::string& key, const std::string& value) {
+    if (resp_code == PROTOCOL_BINARY_RESPONSE_SUCCESS) {
+        mget_result_.get_result_map_[key] = GetResult(resp_code, value);
+    }
+
+    if (caller_loop()) {
+        caller_loop()->RunInLoop(std::bind(mget_callback_, mget_result_));
+    } else {
+        mget_callback_(mget_result_);
+    }
+}
+
+void MultiGetCommand::OnMultiGetCommandOneResponse(int resp_code, const std::string& key, const std::string& value) {
+    LOG_INFO << "OnMultiGetCommandOneResponse " << key << " " << resp_code << " " << value;
+    if (resp_code == PROTOCOL_BINARY_RESPONSE_SUCCESS) {
+        mget_result_.get_result_map_[key] = GetResult(resp_code, value);
+    }
+    mget_result_.code = resp_code;
+}
+
+BufferPtr MultiGetCommand::RequestBuffer() const {
+    BufferPtr buf(new evpp::Buffer(50 * keys_.size()));  // 预分配长度多数情况够长
+
+    for(size_t i = 0; i < keys_.size(); ++i) {
+        protocol_binary_request_header req;
+        bzero((void*)&req, sizeof(req));
+        req.request.magic    = PROTOCOL_BINARY_REQ;
+        if (i < keys_.size() - 1) {
+            req.request.opcode   = PROTOCOL_BINARY_CMD_GETKQ;
+        } else {
+            req.request.opcode   = PROTOCOL_BINARY_CMD_GETK;
+        }
+        req.request.keylen   = htons(keys_[i].size());
+        req.request.datatype = PROTOCOL_BINARY_RAW_BYTES;
+        req.request.vbucket  = vbucket_id();
+        req.request.opaque   = id();
+        req.request.bodylen  = htonl(keys_[i].size());
+
+        buf->Append((void*)&req, sizeof(req));
+        buf->Append(keys_[i].data(), keys_[i].size());
+    }
+
+    return buf;
+}
+
+
+BufferPtr RemoveCommand::RequestBuffer() const {
+    protocol_binary_request_header req;
+    bzero((void*)&req, sizeof(req));
+
+    req.request.magic  = PROTOCOL_BINARY_REQ;
+    req.request.opcode = PROTOCOL_BINARY_CMD_DELETE;
+    req.request.keylen = htons(key_.size());
+    req.request.datatype = PROTOCOL_BINARY_RAW_BYTES;
+    req.request.vbucket  = vbucket_id();
+    req.request.opaque   = id();
+    req.request.bodylen = htonl(static_cast<uint32_t>(key_.size()));
+
+    BufferPtr buf(new evpp::Buffer(sizeof(protocol_binary_request_header) + key_.size()));
+    buf->Append((void*)&req, sizeof(req));
+    buf->Append(key_.data(), key_.size());
+
+    return buf;
+}
+
+}
+
+
---
+++ apps/evmc/main.cc
@@ -0,0 +1,97 @@
+
+#include "evpp/libevent_headers.h"
+#include "evpp/libevent_watcher.h"
+#include "evpp/event_loop.h"
+#include "evpp/event_loop_thread.h"
+#include "evpp/buffer.h"
+#include "evpp/tcp_conn.h"
+#include "evpp/tcp_client.h"
+
+#include <thread>
+
+#include "mctypes.h"
+#include "memcache_client_pool.h"
+
+namespace {
+
+using namespace evmc;
+static void OnTestSetDone(const std::string key, int code) {
+    LOG_INFO << "+++++++++++++ OnTestSetDone code=" << code << " " << key;
+}
+static void OnTestGetDone(const std::string key, GetResult res) {
+    LOG_INFO << "============= OnTestGetDone " << key << " code=" << res.code << " " << res.value;
+}
+static void OnTestRemoveDone(const std::string key, int code) {
+    LOG_INFO << "------------- OnTestRemoveDone code=" << code << " " << key;
+}
+static void OnTestMultiGetDone(MultiGetResult res) {
+    LOG_INFO << ">>>>>>>>>>>>> OnTestMultiGetDone code=" << res.code;
+    std::map<std::string, GetResult>::const_iterator it = res.get_result_map_.begin();
+    for(; it != res.get_result_map_.end(); ++it) {
+        LOG_INFO << ">>>>>>>>>>>>> OnTestMultiGetDone " << it->first << " " << it->second.code << " " << it->second.value;
+    }
+}
+
+static EventLoopPtr g_loop;
+static void StopLoop() {
+    g_loop->Stop();
+}
+
+static void MyEventThread() {
+    LOG_INFO << "EventLoop is running ...";
+    g_loop = EventLoopPtr(new evpp::EventLoop);
+    g_loop->Run();
+}
+
+}
+
+int main() {
+// TEST_UNIT(testMemcacheClient) {
+    srand(time(NULL));
+    std::thread th(MyEventThread);
+
+    MemcacheClientPool mcp("./kill_storage_cluster.json", 32, 200);
+    assert(mcp.Start());
+
+    const static int MAX_KEY = 1000000;
+
+    for(size_t i = 0; i < MAX_KEY; i += 2) {
+        std::stringstream ss_key;
+        ss_key << "test" << i;
+        std::stringstream ss_value;
+        ss_value << "test_value" << i;
+        // usleep(10000);
+        mcp.Set(g_loop, ss_key.str(), ss_value.str(), &OnTestSetDone);
+    }
+
+#if 1
+    for(size_t i = 0; i < MAX_KEY; ++i) {
+        std::stringstream ss;
+        ss << "test" << i;
+        mcp.Get(g_loop, ss.str().c_str(), &OnTestGetDone);
+    }
+
+    std::vector<std::string> mget_keys;
+    for(size_t i = 0; i < MAX_KEY; ++i) {
+        std::stringstream ss;
+        ss << "test" << i;
+        mget_keys.push_back(ss.str());
+        if (mget_keys.size() >= 100) {
+            break;
+        }
+    }
+    mcp.MultiGet(g_loop, mget_keys, &OnTestMultiGetDone);
+
+    for(size_t i = 0; i < MAX_KEY/2; ++i) {
+        std::stringstream ss_key;
+        ss_key << "test" << i;
+        mcp.Remove(g_loop, ss_key.str().c_str(), &OnTestRemoveDone);
+    }
+#endif
+
+    g_loop->RunAfter(1000.0, &StopLoop);
+    th.join();
+    return 0;
+}
+
+
