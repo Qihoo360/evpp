fc9c2a7e8811e7490f8c5c1e4d40bef1d9880b96
weizili <zieckey@gmail.com>
2016-06-26 19:01:18 +0800

    Fix compile errors on Linux

--- include/evpp/libevent_headers.h
+++ include/evpp/libevent_headers.h
@@ -27,6 +27,7 @@
         #include <event2/buffer.h>
         #include <event2/bufferevent.h>
         #include <event2/http.h>
+        #include <event2/http_compat.h>
         #include <event2/http_struct.h>
         #include <event2/event_compat.h>
 	#endif
