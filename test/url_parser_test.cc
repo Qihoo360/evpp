a63cdfa042e9188c0039daa1029c945f13b4b53f
weizili <zieckey@gmail.com>
2016-06-26 11:26:37 +0800

    Add Request::Request(EventLoop* loop, const std::string& url, const std::string& body, Duration timeout), so we can create a Request without ConnPool. And rename Pool to ConnPool.

---
+++ test/url_parser_test.cc
@@ -0,0 +1,31 @@
+#include "test_common.h"
+
+#include <evpp/httpc/url_parser.h>
+
+TEST_UNIT(testURLParser) {
+    struct TestCase {
+        std::string url;
+
+        std::string protocol;
+        std::string host;
+        std::string port;
+        std::string path;
+        std::string query;
+    };
+
+    TestCase cases[] = {
+        { "http://www.so.com/query?a=1", "http", "www.so.com", "80", "/query", "a=1" },
+        { "http://www.so.com/", "http", "www.so.com", "80", "/", "" },
+        { "http://www.so.com", "http", "www.so.com", "80", "", "" },
+    };
+
+    for (size_t i = 0; i < H_ARRAYSIZE(cases); i++) {
+        evpp::httpc::URLParser p(cases[i].url);
+        H_TEST_ASSERT(p.protocol == cases[i].protocol);
+        H_TEST_ASSERT(p.host == cases[i].host);
+        H_TEST_ASSERT(p.port == cases[i].port);
+        H_TEST_ASSERT(p.path == cases[i].path);
+        H_TEST_ASSERT(p.query == cases[i].query);
+    }
+}
+
