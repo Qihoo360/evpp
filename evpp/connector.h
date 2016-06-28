2b17b15e60f231f43f81d11f5545594344508bee
weizili.build9 <zieckey@gmail.com>
2016-06-28 13:41:10 +0800

    Add DNS resolving logic for Connector of TCPClient if possible.

--- evpp/connector.h
+++ evpp/connector.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <vector>
+
 #include "evpp/inner_pre.h"
-#include "duration.h"
+#include "evpp/duration.h"
 
 namespace evpp {
     class EventLoop;
     class FdChannel;
     class TimerEventWatcher;
+    class DNSResolver;
     class EVPP_EXPORT Connector {
     public:
         typedef std::function<void(int sockfd, const std::string&/*local addr*/)> NewConnectionCallback;
@@ -21,15 +24,18 @@ namespace evpp {
         void HandleWrite();
         void HandleError();
         void OnConnectTimeout();
+        void OnDNSResolved(const std::vector <struct in_addr>& addrs);
+        std::string StatusToString() const;
     private:
-        enum Status { kDisconnected, kConnecting, kConnected };
+        enum Status { kDisconnected, kDNSResolving, kDNSResolved, kConnecting, kConnected };
         Status status_;
         EventLoop* loop_;
-        std::string remote_addr_;
+        std::string remote_addr_; // host:port
         struct sockaddr_in raddr_;
         Duration timeout_;
         std::shared_ptr<FdChannel> chan_;
         std::shared_ptr<TimerEventWatcher> timer_;
+        std::shared_ptr<DNSResolver> dns_resolver_;
         NewConnectionCallback conn_fn_;
     };
 }
