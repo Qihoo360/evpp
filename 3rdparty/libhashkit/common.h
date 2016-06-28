7dfc6b5201dd076540eeae058b23c40e119706b8
muyuwei <muyuwei@build19.kill.corp.qihoo.net>
2016-06-28 20:23:48 +0800

    add 3rd party dependency

---
+++ 3rdparty/libhashkit/common.h
@@ -0,0 +1,34 @@
+/* HashKit
+ * Copyright (C) 2009 Brian Aker
+ * All rights reserved.
+ *
+ * Use and distribution licensed under the BSD license.  See
+ * the COPYING file in the parent directory for full text.
+ */
+
+#ifndef HASHKIT_COMMON_H
+#define HASHKIT_COMMON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "hashkit.h"
+
+HASHKIT_LOCAL
+void md5_signature(const unsigned char *key, unsigned int length, unsigned char *result);
+
+HASHKIT_LOCAL
+int update_continuum(hashkit_st *hashkit);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HASHKIT_COMMON_H */
