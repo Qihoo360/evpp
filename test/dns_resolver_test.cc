3696c1a8d321b7d770355ee04093aac3c8c871a6
weizili.build9 <zieckey@gmail.com>
2016-06-28 13:39:48 +0800

    Add DNSResolver and test code

---
+++ test/dns_resolver_test.cc
@@ -0,0 +1,30 @@
+#include "test_common.h"
+
+#include <evpp/libevent_headers.h>
+#include <evpp/libevent_watcher.h>
+#include <evpp/event_loop.h>
+#include <evpp/event_loop_thread.h>
+#include <evpp/timestamp.h>
+#include <evpp/dns_resolver.h>
+#include <atomic>
+
+namespace {
+    static bool resolved = false;
+    static void OnResolved(const std::vector <struct in_addr>& addrs) {
+        resolved = true;
+    }
+}
+
+
+TEST_UNIT(testDNSResolver) {
+    evpp::Duration delay(double(1.0)); // 1s
+    evpp::EventLoopThread t;
+    t.Start();
+    evpp::Timestamp begin = evpp::Timestamp::Now();
+    evpp::DNSResolver dns_resolver(t.event_loop(), "www.so.com", evpp::Duration(1.0), &OnResolved);
+    dns_resolver.Start();
+    while (!resolved) {
+        usleep(1);
+    }
+    t.Stop(true);
+}
