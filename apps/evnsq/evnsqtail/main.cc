751cdc09b0c8305c4532fca6c1e9189ecfa286fb
weizili.build9 <zieckey@gmail.com>
2016-06-29 21:39:38 +0800

    Add Consumer::ConnectToLoopupds and Consumer::ConnectToLoopupd

--- apps/evnsq/evnsqtail/main.cc
+++ apps/evnsq/evnsqtail/main.cc
@@ -23,17 +23,27 @@ void OnConnection(const evpp::TCPConnPtr& conn) {
 }
 
 int main(int argc, char* argv[]) {
-    //std::string addr = "127.0.0.1:4150";
-    //std::string  addr = "10.16.28.17:4150";
-    std::string addr = "weizili-L1:4150";
+    std::string nsqd_tcp_addr;
+    std::string lookupd_http_url;
+    //nsqd_tcp_addr = "127.0.0.1:4150";
+    //nsqd_tcp_addr = "10.16.28.17:4150";
+    //nsqd_tcp_addr = "weizili-L1:4150";
+    lookupd_http_url = "http://127.0.0.1:4161/lookup?topic=test";
     if (argc == 2) {
-        addr = argv[1];
+        if (strncmp(argv[1], "http", 4) == 0) {
+            lookupd_http_url = argv[1];
+        } else {
+            nsqd_tcp_addr = argv[1];
+        }
     }
     evpp::EventLoop loop;
     evnsq::Consumer client(&loop, "test", "ch1", evnsq::Option());
     client.SetMessageCallback(&OnMessage);
-//     client.SetConnectionCallback(&OnConnection);
-    client.ConnectToNSQDs(addr);
+    if (!nsqd_tcp_addr.empty()) {
+        client.ConnectToNSQDs(nsqd_tcp_addr);
+    } else {
+        client.ConnectToLoopupds(lookupd_http_url);
+    }
     loop.Run();
     return 0;
 }
