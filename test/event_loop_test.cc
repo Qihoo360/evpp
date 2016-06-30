639986df14d422e69bd228f33da7c0d0746c92cb
weizili <zieckey@gmail.com>
2016-06-30 15:54:38 +0800

    Add EventLoop::RunEvery

--- test/event_loop_test.cc
+++ test/event_loop_test.cc
@@ -12,9 +12,10 @@
 namespace evloop {
     static std::shared_ptr<evpp::EventLoop> loop;
     static evpp::Duration delay(1.0);
-    static bool g_event_handler_called = false;
-    static void Handle() {
-        g_event_handler_called = true;
+    static bool event_handler_called = false;
+    static void Handle(evpp::InvokeTimerPtr t) {
+        event_handler_called = true;
+        t->Cancel();
         loop->Stop();
     }
 
@@ -23,6 +24,12 @@ namespace evloop {
         loop = std::shared_ptr<evpp::EventLoop>(new evpp::EventLoop);
         loop->Run();
     }
+
+    static int periodic_run_count = 0;
+    static void PeriodicFunc() {
+        periodic_run_count++;
+        LOG_INFO << "PeriodicFunc is called , periodic_run_count=" << periodic_run_count;
+    }
 }
 
 TEST_UNIT(testEventLoop) {
@@ -30,11 +37,13 @@ TEST_UNIT(testEventLoop) {
     std::thread th(MyEventThread);
     usleep(delay.Microseconds());
     evpp::Timestamp start = evpp::Timestamp::Now();
-    loop->RunAfter(delay, &Handle);
+    evpp::InvokeTimerPtr t = loop->RunEvery(evpp::Duration(0.3), &PeriodicFunc);
+    loop->RunAfter(delay, std::bind(&Handle, t));
     th.join();
     evpp::Duration cost = evpp::Timestamp::Now() - start;
     H_TEST_ASSERT(delay <= cost);
-    H_TEST_ASSERT(g_event_handler_called);
+    H_TEST_ASSERT(event_handler_called);
+    H_TEST_ASSERT(periodic_run_count == 3);
 }
 
 
