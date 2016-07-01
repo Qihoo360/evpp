f9fc561db5cfff4638123fbe3228fb47d24a81f6
weizili.build9 <zieckey@gmail.com>
2016-07-01 13:45:23 +0800

    Fix event_add calling twice problem bug

--- evpp/libevent_compact.cc
+++ evpp/libevent_compact.cc
@@ -2,19 +2,19 @@
 #include "evpp/libevent_headers.h"
 
 #ifdef _LIBEVENT14
-struct event * event_new(struct event_base *base, int fd, short events,
+struct event* event_new(struct event_base *base, int fd, short events,
     void(*cb)(int, short, void *), void *arg) {
     struct event *ev;
     ev = (struct event*)malloc(sizeof(struct event));
     if (ev == NULL)
-        return (NULL);
-    event_base_set(base, ev);
-    event_set(ev, fd, events, cb, arg);
-    return (ev);
+        return NULL;
+    ::event_set(ev, fd, events, cb, arg);
+    ::event_base_set(base, ev);
+    return ev;
 }
 
 void event_free(struct event *ev) {
-    event_del(ev);
+    ::event_del(ev);
     free(ev);
 }
 
