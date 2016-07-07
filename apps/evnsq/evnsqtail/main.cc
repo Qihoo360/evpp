c69b5eaabba08583073a9e831e714b65557991e6
weizili <zieckey@gmail.com>
2016-07-07 22:04:37 +0800

    Refactor Consumer : derived from Client

--- apps/evnsq/evnsqtail/main.cc
+++ apps/evnsq/evnsqtail/main.cc
@@ -11,10 +11,10 @@ int OnMessage(const evnsq::Message* msg) {
 int main(int argc, char* argv[]) {
     std::string nsqd_tcp_addr;
     std::string lookupd_http_url;
-    //nsqd_tcp_addr = "127.0.0.1:4150";
+    nsqd_tcp_addr = "127.0.0.1:4150";
     //nsqd_tcp_addr = "10.16.28.17:4150";
     //nsqd_tcp_addr = "weizili-L1:4150";
-    lookupd_http_url = "http://127.0.0.1:4161/lookup?topic=test";
+    //lookupd_http_url = "http://127.0.0.1:4161/lookup?topic=test";
     if (argc == 2) {
         if (strncmp(argv[1], "http", 4) == 0) {
             lookupd_http_url = argv[1];
