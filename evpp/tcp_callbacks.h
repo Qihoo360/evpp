23fb82817ace62ecf8c730a48340f15e7b2d520f
weizili <zieckey@gmail.com>
2016-07-08 11:16:02 +0800

    Add comments for TCPConn::ConnectionCallback of TCPClient and TCPServer. Change TCPClient::auto_reconnect_ from int to bool. Add TCPServer::SetConnectionCallback

--- evpp/tcp_callbacks.h
+++ evpp/tcp_callbacks.h
@@ -10,7 +10,11 @@ namespace evpp {
 
     typedef std::shared_ptr<TCPConn> TCPConnPtr;
     typedef std::function<void()> TimerCallback;
+
+    // 连接成功、连接断开、连接失败等事件，都会调用该回调
     typedef std::function<void(const TCPConnPtr&)> ConnectionCallback;
+
+
     typedef std::function<void(const TCPConnPtr&)> CloseCallback;
     typedef std::function<void(const TCPConnPtr&)> WriteCompleteCallback;
     typedef std::function<void(const TCPConnPtr&, size_t)> HighWaterMarkCallback;
--- evpp/tcp_server.h
+++ evpp/tcp_server.h
@@ -23,9 +23,18 @@ namespace evpp {
         ~TCPServer();
         bool Start();
         void Stop();
+
+        // 设置一个连接相关的回调函数，当接收到一个新的连接、或已有连接断开等事件发生时，都会调用该回调
+        //  当成功建立连接时，回调中的参数 TCPConn::IsConnected() == true
+        //  当连接断开时，回调中的参数 TCPConn::IsDisconnecting() == true
+        void SetConnectionCallback(const ConnectionCallback& cb) {
+            conn_fn_ = cb;
+        }
+
         void SetMessageCallback(MessageCallback cb) {
             msg_fn_ = cb;
         }
+
         void SetThreadDispatchPolicy(ThreadDispatchPolicy v) {
             threads_dispatch_policy_ = v;
         }
