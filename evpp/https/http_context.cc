021ad59b5a7260834a544786541842314aaed156
weizili <zieckey@gmail.com>
2016-07-11 10:35:34 +0800

    Add HTTPContext::FindRequestHeader, finished HTTPContext uri parsing code, refactor URLParser code

--- evpp/https/http_context.cc
+++ evpp/https/http_context.cc
@@ -32,39 +32,15 @@ namespace evpp {
 #endif
             }
 
-#if 0
-            //TODO refactor this code
-            const stringset& request_header_keys_for_parsing = hsrv->request_header_keys_for_parsing();
-
-            if (hsrv->parse_parameters() || request_header_keys_for_parsing.size() > 0) {
-                this->params = new HTTPParameterMap;
+#if LIBEVENT_VERSION_NUMBER >= 0x02001500
+            uri = evhttp_uri_get_path(req->uri_elems);
+#else
+            const char* p = strchr(req->uri, '?');
+            if (p != NULL) {
+                uri = std::string(req->uri, p - req->uri);
+            } else {
+                uri = req->uri;
             }
-
-            bool rc = osl::URIParser::parse(this->req->uri, strlen(this->req->uri), this->uri, this->params);
-
-            if (this->params) {
-                static const std::string __s_clientip = "clientip";
-                HTTPParameterMap::const_iterator it = this->params->find(__s_clientip);
-                if (it != this->params->end()) {
-                    remote_ip = it->second;
-                }
-            }
-
-
-            if (request_header_keys_for_parsing.size() > 0) {
-                assert(this->params);
-                stringset::const_iterator it(request_header_keys_for_parsing.begin());
-                stringset::const_iterator ite(request_header_keys_for_parsing.end());
-                for (; it != ite; ++it) {
-                    const std::string& key = *it;
-                    struct evkeyvalq *headers = req->input_headers;
-                    const char* value = evhttp_find_header(headers, key.c_str());
-                    if (value) {
-                        (*this->params)[key] = value;
-                    }
-                }
-            }
-            return rc;
 #endif
 
             return true;
@@ -74,28 +50,12 @@ namespace evpp {
             return req->uri;
         }
 
-        const std::string& HTTPContext::GetRequestHeader(const std::string& key, bool* found) {
-            if (this->params) {
-                HTTPParameterMap::const_iterator it = this->params->find(key);
-                if (it != this->params->end()) {
-                    if (found) {
-                        *found = true;
-                    }
-                    return it->second;
-                }
-            }
-
-
-            if (found) {
-                *found = false;
-            }
-
-            return __s_null;
-        }
-
         void HTTPContext::AddResponseHeader(const std::string& key, const std::string& value) {
             evhttp_add_header(req->output_headers, key.data(), value.data());
         }
 
+        const char* HTTPContext::FindRequestHeader(const char* key) {
+            return evhttp_find_header(req->input_headers, key);
+        }
     }
 }
