a4d03ab527225ce6c33f148ae41d5cb57540d401
weizili.build9 <zieckey@gmail.com>
2016-07-12 16:24:50 +0800

    Add logic code to process the NSQ published response code OK

--- apps/evnsq/producer.cc
+++ apps/evnsq/producer.cc
@@ -7,8 +7,13 @@
 namespace evnsq {
 
     Producer::Producer(evpp::EventLoop* loop, const Option& ops)
-        : Client(loop, kProducer, "", "", ops) {
+        : Client(loop, kProducer, "", "", ops)
+        , wait_ack_count_(0)
+        , published_count_(0)
+        , published_ok_count_(0)
+        , published_failed_count_(0) {
         conn_ = conns_.end();
+        ready_to_publish_fn_ = std::bind(&Producer::OnReady, this, std::placeholders::_1);
     }
 
     Producer::~Producer() {
@@ -21,9 +26,55 @@ namespace evnsq {
         }
         assert(conn_ != conns_.end());
         assert(conn_->second->IsReady());
-        Command c;
-        c.Publish(topic, msg);
-        conn_->second->WriteCommand(&c); //TODO need to process the response code 'OK'
-        ++conn_;
+        Command* c = new Command;
+        c->Publish(topic, msg);
+        published_count_++;
+        conn_->second->WriteCommand(c);
+        PushWaitACKCommand(conn_->second.get(), c);
+        LOG_INFO << "Publish a message, command=" << c;
+        
+        ++conn_; // Using next Conn
     }
+
+    void Producer::OnReady(Conn* conn) {
+        conn->SetPublishResponseCallback(std::bind(&Producer::OnPublishResponse, this, conn, std::placeholders::_1, std::placeholders::_2));
+        if (ready_fn_) {
+            ready_fn_();
+        }
+    }
+
+    void Producer::OnPublishResponse(Conn* conn, const char* d, size_t len) {
+        Command* c = PopWaitACKCommand(conn);
+        if (len == 2 && d[0] == 'O' && d[1] == 'K') {
+            LOG_INFO << "Get a PublishResponse message OK, command=" << c;
+            published_ok_count_++;
+            delete c;
+            return;
+        }
+
+        published_failed_count_++;
+        LOG_ERROR << "Publish command " << c << " failed : [" << std::string(d, len) << "]. Try again.";
+        conn_->second->WriteCommand(c);
+        PushWaitACKCommand(conn_->second.get(), c);
+    }
+
+    Command* Producer::PopWaitACKCommand(Conn* conn) {
+        CommandList& cl = wait_ack_[conn];
+        assert(!cl.first.empty());
+        Command* c = *cl.first.begin();
+        cl.first.pop_front();
+        cl.second--;
+        wait_ack_count_--;
+        assert(cl.first.size() == cl.second);
+        return c;
+    }
+
+    void Producer::PushWaitACKCommand(Conn* conn, Command* cmd) {
+        CommandList& cl = wait_ack_[conn];
+        cl.first.push_back(cmd);
+        cl.second++;
+        wait_ack_count_++;
+        assert(cl.first.size() == cl.second);
+    }
+
 }
