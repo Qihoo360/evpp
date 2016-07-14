35cbce2a3f36ae960cf866b9ddfb9ff968f23baa
weizili <zieckey@gmail.com>
2016-07-14 16:42:41 +0800

    Refactor : add DNSResolver::SyncDNSResolve

--- evpp/dns_resolver.cc
+++ evpp/dns_resolver.cc
@@ -25,7 +25,12 @@ void DNSResolver::StartInLoop() {
     LOG_INFO << "DNSResolver::StartInLoop tid=" << std::this_thread::get_id() << " this=" << this;
     loop_->AssertInLoopThread();
 
+    SyncDNSResolve();
 
+    //TODO Implement asynchronous DNS resolve
+}
+
+void DNSResolver::SyncDNSResolve() {
     /* Build the hints to tell getaddrinfo how to act. */
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
@@ -33,10 +38,6 @@ void DNSResolver::StartInLoop() {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP; /* We want a TCP socket */
     hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
-    hints.ai_protocol = 0;          /* Any protocol */
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
 
     /* Look up the hostname. */
     struct addrinfo* answer = NULL;
@@ -58,7 +59,19 @@ void DNSResolver::StartInLoop() {
     }
 
     functor_(this->addrs_);
-    //TODO Implement asynchronous DNS resolve
+}
+
+
+void DNSResolver::AsyncDNSResolve() {
+    /* Build the hints to tell getaddrinfo how to act. */
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; /* v4 or v6 is fine. */
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP; /* We want a TCP socket */
+    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+
+    //TODO
 }
 
 void DNSResolver::Cancel() {
@@ -83,98 +96,4 @@ void DNSResolver::OnTimeout() {
 void DNSResolver::OnCanceled() {
     LOG_INFO << "DNSResolver::OnCanceled tid=" << std::this_thread::get_id() << " this=" << this;
 }
-
-/**
-* The callback that contains the results from a lookup.
-* - type is either DNS_IPv4_A or DNS_PTR or DNS_IPv6_AAAA
-* - count contains the number of addresses of form type
-* - ttl is the number of seconds the resolution may be cached for.
-* - addresses needs to be cast according to type
-*/
-void DNSResolver::OnResolved(int result, char type, int count, int ttl, void* addresses) {
-    int dns_ok = 0;
-    int dns_err = 0;
-
-    if (result == DNS_ERR_TIMEOUT) {
-        LOG_WARN << "DNS Resolved [Timed out]";
-        dns_err = result;
-        goto out;
-    }
-
-    if (result != DNS_ERR_NONE) {
-        LOG_WARN << "DNS Resolved Error code " << result;
-        goto out;
-    }
-
-    LOG_TRACE << "type=" << type << " count=" << count << " ttl=" << ttl;
-
-    switch (type) {
-    case DNS_IPv6_AAAA: {
-#if defined(HAVE_STRUCT_IN6_ADDR) && defined(HAVE_INET_NTOP) && defined(INET6_ADDRSTRLEN)
-        struct in6_addr* in6_addrs = addresses;
-        char buf[INET6_ADDRSTRLEN + 1];
-        int i;
-
-        /* a resolution that's not valid does not help */
-        if (ttl < 0) {
-            goto out;
-        }
-
-        for (i = 0; i < count; ++i) {
-            const char* b = inet_ntop(AF_INET6, &in6_addrs[i], buf, sizeof(buf));
-
-            if (b) {
-                fprintf(stderr, "%s ", b);
-            } else {
-                fprintf(stderr, "%s ", strerror(errno));
-            }
-        }
-
-#endif
-        break;
-    }
-
-    case DNS_IPv4_A: {
-        struct in_addr* in_addrs = (struct in_addr*)addresses;
-
-        /* a resolution that's not valid does not help */
-        if (ttl < 0) {
-            goto out;
-        }
-
-        for (int i = 0; i < count; ++i) {
-            this->addrs_.push_back(in_addrs[i]);
-            fprintf(stderr, "%s ", inet_ntoa(in_addrs[i]));
-        }
-
-        break;
-    }
-
-    case DNS_PTR:
-
-        /* may get at most one PTR */
-        if (count != 1) {
-            goto out;
-        }
-
-        fprintf(stderr, "%s ", *(char**)addresses);
-        break;
-
-    default:
-        goto out;
-    }
-
-    dns_ok = type;
-    (void)dns_ok;
-    (void)dns_err;
-out:
-    this->functor_(this->addrs_);
-    evdns_shutdown(0);
-}
-
-void DNSResolver::OnResolved(int result, char type, int count, int ttl, void* addresses, void* arg) {
-    DNSResolver* thiz = (DNSResolver*)arg;
-    thiz->OnResolved(result, type, count, ttl, addresses);
-}
-
 }
