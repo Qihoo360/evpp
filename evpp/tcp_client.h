811836b89569d748ac975f442cfe746fc8ec2376
weizili <zieckey@gmail.com>
2016-07-15 21:08:47 +0800

    Change some shared_ptr to unique_ptr

--- evpp/tcp_client.h
+++ evpp/tcp_client.h
@@ -68,7 +68,7 @@ private:
     mutable std::mutex mutex_; // The guard of conn_
     TCPConnPtr conn_;
 
-    std::shared_ptr<Connector> connector_;
+    std::unique_ptr<Connector> connector_;
     Duration connecting_timeout_; // Default : 3 seconds
 
     ConnectionCallback conn_fn_;
