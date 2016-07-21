7a2af571a89f57d6ef3d091d159fbc7855299b7b
weizili.build9 <zieckey@gmail.com>
2016-07-21 21:29:30 +0800

    Add EventLoopThreadPool for udp server

--- evpp/udp/udp_server.h
+++ evpp/udp/udp_server.h
@@ -6,10 +6,15 @@
 #include <thread>
 
 namespace evpp {
+class EventLoopThreadPool;
 namespace udp {
 class EVPP_EXPORT Server {
 public:
     typedef std::function<void(const MessagePtr& msg)> MessageHandler;
+    enum ThreadDispatchPolicy {
+        kRoundRobin,
+        kIPAddressHashing,
+    };
 public:
     Server();
     ~Server();
@@ -28,12 +33,23 @@ public:
         message_handler_ = handler;
     }
 
+    void SetEventLoopThreadPool(std::shared_ptr<EventLoopThreadPool>& pool) {
+        tpool_ = pool;
+    }
+
+    void SetThreadDispatchPolicy(ThreadDispatchPolicy v) {
+        threads_dispatch_policy_ = v;
+    }
 private:
     class RecvThread;
     typedef std::shared_ptr<RecvThread> RecvThreadPtr;
     std::vector<RecvThreadPtr> recv_threads_;
 
     MessageHandler   message_handler_;
+
+    // 工作线程池，处理请求
+    std::shared_ptr<EventLoopThreadPool> tpool_;
+    ThreadDispatchPolicy threads_dispatch_policy_;
 private:
     void RecvingLoop(RecvThread* th);
 };
