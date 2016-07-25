25c11272a6ce334afe0062442a90e38355546769
weizili.build9 <zieckey@gmail.com>
2016-07-25 18:35:23 +0800

    Fix testDNSResolver and testTCPServer1 test failed bug : Fix EventLoopThread::IsRunning bug

--- test/tcp_server_test.cc
+++ test/tcp_server_test.cc
@@ -41,27 +41,28 @@ std::shared_ptr<evpp::TCPClient> StartTCPClient(evpp::EventLoop* loop) {
     client->SetConnectionCallback(&OnClientConnection);
     client->Connect();
     loop->RunAfter(evpp::Duration(1.0), std::bind(&evpp::TCPClient::Disconnect, client));
-    loop->RunAfter(evpp::Duration(1.1), std::bind(&evpp::EventLoop::Stop, loop));
     return client;
 }
 }
 
 
 TEST_UNIT(testTCPServer1) {
-    evpp::EventLoopThread t;
-    t.Start();
+    evpp::EventLoopThread tcp_client_thread;
+    tcp_client_thread.SetName("TCPClientThread");
+    tcp_client_thread.Start();
     evpp::EventLoop loop;
     evpp::TCPServer tsrv(&loop, addr, "tcp_server", 2);
     tsrv.SetMessageCallback(&OnMessage);
     tsrv.Start();
     loop.RunAfter(evpp::Duration(1.4), std::bind(&StopTCPServer, &tsrv));
     loop.RunAfter(evpp::Duration(1.6), std::bind(&evpp::EventLoop::Stop, &loop));
-    std::shared_ptr<evpp::TCPClient> client = StartTCPClient(t.event_loop());
+    std::shared_ptr<evpp::TCPClient> client = StartTCPClient(tcp_client_thread.event_loop());
     loop.Run();
-    t.Stop(true);
+    tcp_client_thread.Stop(true);
+    H_TEST_ASSERT(!loop.running());
+    H_TEST_ASSERT(tcp_client_thread.IsStopped());
     H_TEST_ASSERT(connected);
     H_TEST_ASSERT(message_recved);
-    ::usleep(evpp::Duration(1.0).Microseconds());
 }
 
 TEST_UNIT(testTCPServerSilenceShutdown) {
