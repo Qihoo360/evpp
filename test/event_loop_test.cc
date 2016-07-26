2d4edfc4071a2350050216da05e9631cc8ceae45
weizili.build9 <zieckey@gmail.com>
2016-07-26 14:38:22 +0800

    Fix testEventLoop

--- test/event_loop_test.cc
+++ test/event_loop_test.cc
@@ -23,6 +23,7 @@ static void MyEventThread() {
     LOG_INFO << "EventLoop is running ...";
     loop = std::shared_ptr<evpp::EventLoop>(new evpp::EventLoop);
     loop->Run();
+    loop.reset(); // 确保析构时，是在其自身运行的线程中
 }
 
 static int periodic_run_count = 0;
