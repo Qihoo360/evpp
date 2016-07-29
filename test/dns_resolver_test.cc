433f4b0fe243b89c9cb301f2399e0cc6c45628dd
weizili <zieckey@gmail.com>
2016-07-29 20:30:15 +0800

    Fix testDNSResolver test failed

--- test/dns_resolver_test.cc
+++ test/dns_resolver_test.cc
@@ -7,9 +7,16 @@
 
 namespace {
 static bool resolved = false;
+static bool deleted = false;
 static void OnResolved(const std::vector <struct in_addr>& addrs) {
     resolved = true;
 }
+
+static void DeleteDNSResolver(evpp::DNSResolver* r) {
+    delete r;
+    deleted = true;
+}
+
 }
 
 
@@ -17,10 +24,15 @@ TEST_UNIT(testDNSResolver) {
     evpp::Duration delay(double(1.0)); // 1s
     std::unique_ptr<evpp::EventLoopThread> t(new evpp::EventLoopThread);
     t->Start(true);
-    evpp::DNSResolver dns_resolver(t->event_loop(), "www.so.com", evpp::Duration(1.0), &OnResolved);
-    dns_resolver.Start();
+    evpp::DNSResolver* dns_resolver(new evpp::DNSResolver(t->event_loop(), "www.so.com", evpp::Duration(1.0), &OnResolved));
+    dns_resolver->Start();
 
     while (!resolved) {
+        usleep(1);
+    }
+
+    t->event_loop()->RunInLoop(std::bind(&DeleteDNSResolver, dns_resolver));
+    while (!deleted) {
         usleep(1);
     }
 
