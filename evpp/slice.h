4aed8d6424402e10537abbec4e7586213a5510dc
weizili.build9 <zieckey@gmail.com>
2016-07-30 12:25:59 +0800

    Add copyright for the 3rdparty code

--- evpp/slice.h
+++ evpp/slice.h
@@ -1,3 +1,17 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// Slice is a simple structure containing a pointer into some external
+// storage and a size.  The user of a Slice must ensure that the slice
+// is not used after the corresponding external storage has been
+// deallocated.
+//
+// Multiple threads can invoke const methods on a Slice without
+// external synchronization, but if any of the threads may call a
+// non-const method, all threads accessing the same Slice must use
+// external synchronization.
+
 #pragma once
 
 #include <string.h>
@@ -5,6 +19,10 @@
 #include <string>
 
 namespace evpp {
+
+// Copy from leveldb project 
+// @see https://github.com/google/leveldb/blob/master/include/leveldb/slice.h
+
 class Slice {
 public:
     typedef char value_type;
