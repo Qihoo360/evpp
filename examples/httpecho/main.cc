4226ba02c18e9cc9bad3afac1a612627725ef5f9
weizili <zieckey@gmail.com>
2016-07-30 10:50:46 +0800

    Add server.RegisterHandler("/echo", &RequestHandler);

--- examples/httpecho/main.cc
+++ examples/httpecho/main.cc
@@ -5,13 +5,19 @@
 #include "../echo/winmain-inl.h"
 #endif
 
-void RequestHandler(const evpp::http::ContextPtr& ctx, const evpp::http::HTTPSendResponseCallback& cb) {
+void DefaultHandler(const evpp::http::ContextPtr& ctx, const evpp::http::HTTPSendResponseCallback& cb) {
     std::stringstream oss;
     oss << "func=" << __FUNCTION__ << " OK"
         << " ip=" << ctx->remote_ip << "\n"
         << " uri=" << ctx->uri << "\n"
         << " body=" << ctx->body.ToString() << "\n";
+    ctx->AddResponseHeader("Content-Type", "application/octet-stream");
+    ctx->AddResponseHeader("Server", "evpp");
     cb(oss.str());
+}
+
+void RequestHandler(const evpp::http::ContextPtr& ctx, const evpp::http::HTTPSendResponseCallback& cb) {
+    cb(ctx->body.ToString());
 }
 
 int main(int argc, char* argv[]) {
@@ -20,9 +26,10 @@ int main(int argc, char* argv[]) {
     int thread_num = 0;
 
     if (argc > 1) {
-        if (std::string("-h") == argv[1] || std::string("--h") == argv[1] || std::string("--help") == argv[1] || std::string("-help") == argv[1]) {
+        if (std::string("-h") == argv[1] || std::string("--h") == argv[1] || std::string("-help") == argv[1] || std::string("--help") == argv[1]) {
             std::cout << "usage : " << argv[0] << " <listen_port> <thread_num>\n";
-            std::cout << " e.g. : " << argv[0] << " 80 24\n";
+            std::cout << " e.g. : " << argv[0] << " 8080 24\n";
+            return 0;
         }
     }
 
@@ -36,7 +43,8 @@ int main(int argc, char* argv[]) {
         thread_num = atoi(argv[2]);
     }
     evpp::http::HTTPServer server(thread_num);
-    server.RegisterDefaultHandler(&RequestHandler);
+    server.RegisterDefaultHandler(&DefaultHandler);
+    server.RegisterHandler("/echo", &RequestHandler);
     server.Start(ports);
     while (!server.IsStopped()) {
         usleep(1);
