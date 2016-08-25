6f266cdd49b74ce621f0a4013a51ff4ae0128c6e
weizili <zieckey@gmail.com>
2016-08-25 11:20:41 +0800

    Fix compile error

--- test/test_common.h
+++ test/test_common.h
@@ -3,24 +3,24 @@
 
 #include <gtest/gtest.h>
 
-/**
-*    How to write a unit test:
-*    Please see <code>*_test.cc</code>
-*
-*    Usage:
-*
-*    TEST_UNIT(sample_test)
-*    {
-*        int a = 1;
-*        int b = a;
-*        H_TEST_ASSERT(a == b);
-*        int c = a + b;
-*        H_TEST_ASSERT(c == a + b);
-*
-*        H_TEST_ASSERT(printf("Please using H_TEST_ASSERT instead of assert, H_ASSERT, CPPUNIT_ASSERT!\n"));
-*        H_TEST_ASSERT(false || printf("If assert failed, we can use printf to print this some error message, because printf return int!\n"));
-*    }
-*/
+//
+//   How to write a unit test:
+//   Please see <code>*_test.cc</code>
+//
+//   Usage:
+//
+//   TEST_UNIT(sample_test)
+//   {
+//       int a = 1;
+//       int b = a;
+//       H_TEST_ASSERT(a == b);
+//       int c = a + b;
+//       H_TEST_ASSERT(c == a + b);
+//
+//       H_TEST_ASSERT(printf("Please using H_TEST_ASSERT instead of assert, H_ASSERT, CPPUNIT_ASSERT!\n"));
+//       H_TEST_ASSERT(false || printf("If assert failed, we can use printf to print this some error message, because printf return int!\n"));
+//   }
+//
 
 #define TEST_UNIT(name)  \
     class GtestObjectClass_##name : public testing::Test{ \
