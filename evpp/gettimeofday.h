f110453b715b12fdea0ebcaad0bba374e554035d
weizili.build9 <zieckey@gmail.com>
2016-08-25 11:50:00 +0800

    Add H_GETTIMEOFDAY

--- evpp/gettimeofday.h
+++ evpp/gettimeofday.h
@@ -12,68 +12,8 @@
 
 #ifdef WIN32
 
-//thrift-0.8.0 implementation
-#if 0
-
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
-#   define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
-#else
-#   define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
-#endif
-
-struct timezone {
-    int  tz_minuteswest; /* minutes W of Greenwich */
-    int  tz_dsttime;     /* type of dst correction */
-};
-
-inline int gettimeofday(struct timeval* tv, struct timezone* tz) {
-    FILETIME         ft;
-    unsigned __int64 tmpres(0);
-    static int       tzflag;
-
-    if (NULL != tv) {
-        GetSystemTimeAsFileTime(&ft);
-
-        tmpres |= ft.dwHighDateTime;
-        tmpres <<= 32;
-        tmpres |= ft.dwLowDateTime;
-
-        /*converting file time to unix epoch*/
-        tmpres -= DELTA_EPOCH_IN_MICROSECS;
-        tmpres /= 10;  /*convert into microseconds*/
-        tv->tv_sec = (long)(tmpres / 1000000UL);
-        tv->tv_usec = (long)(tmpres % 1000000UL);
-    }
-
-    if (NULL != tz) {
-        if (!tzflag) {
-            _tzset();
-            tzflag++;
-        }
-
-        long time_zone(0);
-        errno_t err(_get_timezone(&time_zone));
-
-        if (err == NO_ERROR) {
-            tz->tz_minuteswest = time_zone / 60;
-        } else {
-            return -1;
-        }
-
-        int day_light(0);
-        err = (_get_daylight(&day_light));
-
-        if (err == NO_ERROR) {
-            tz->tz_dsttime = day_light;
-            return 0;
-        } else {
-            return -1;
-        }
-    }
-
-    return -1;
-}
-#else
+#ifndef H_GETTIMEOFDAY
+#define H_GETTIMEOFDAY
 inline int gettimeofday(struct timeval* tp, void* tzp) {
     time_t clock;
     struct tm tm;
@@ -91,7 +31,7 @@ inline int gettimeofday(struct timeval* tp, void* tzp) {
     tp->tv_usec = wtm.wMilliseconds * 1000;
     return (0);
 }
-#endif
+#endif // end of H_GETTIMEOFDAY
 
 #endif //end of WIN32
 
