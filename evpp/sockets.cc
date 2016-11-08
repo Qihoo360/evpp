d3b169a858b71605256057066bc7b7b9c327fa84
weizili <zieckey@gmail.com>
2016-11-08 20:31:22 +0800

    Add TCPClient::reconnect_interval_

--- evpp/sockets.cc
+++ evpp/sockets.cc
@@ -93,11 +93,10 @@ struct sockaddr_in ParseFromIPPort(const char* address/*ip:port*/) {
 
     if (::inet_pton(AF_INET, a.data(), &addr.sin_addr) <= 0) {
         int serrno = errno;
-
         if (serrno == 0) {
             LOG_INFO << "[" << a.data() << "] is not a IP address. Maybe it is a hostname.";
         } else {
-            LOG_ERROR << "ParseFromIPPort " << strerror(serrno);
+            LOG_ERROR << "ParseFromIPPort failed : [" << a.data() << "] " << strerror(serrno);
         }
     }
 
