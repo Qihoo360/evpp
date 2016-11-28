fcd879e852c4ba1ab261ef5214d62d01fa6d14aa
zhangke-s <zhangke-s@360.cn>
2016-11-28 15:48:27 +0800

    add serial command

--- apps/evmc/memcache_client.cc
+++ apps/evmc/memcache_client.cc
@@ -2,6 +2,7 @@
 
 #include "binary_codec.h"
 #include "memcache_client_pool.h"
+#include "likely.h"
 
 namespace evmc {
 
@@ -10,87 +11,74 @@ MemcacheClient::~MemcacheClient() {
     delete codec_;
 }
 
-void MemcacheClient::PushRunningCommand(CommandPtr cmd) {
-    exec_loop_->AssertInLoopThread();
-
-    if (cmd->id() == 0) {
-        cmd->set_id(next_id());
-    }
-
-    if (!cmd) {
+void MemcacheClient::PushRunningCommand(CommandPtr& cmd) {
+    //exec_loop_->AssertInLoopThread();
+    if (UNLIKELY(!cmd)) {
         return;
     }
 
-    running_command_.push(cmd);
-
-    if (!timeout_.IsZero()) {
-        if (cmd_timer_) {
-            cmd_timer_->Cancel();
-            //LOG_DEBUG << "InvokeTimer canceled for " << " " << conn()->remote_addr();
-        }
-
-        evpp::InvokeTimerPtr timer = exec_loop_->RunAfter(timeout_,
-                                                          std::bind(&MemcacheClient::OnPacketTimeout, shared_from_this(), cmd->id()));
-
-        cmd_timer_ = timer;
+    if (LIKELY(cmd->id() == 0)) {
+        cmd->set_id(next_id());
     }
+    running_command_.emplace(cmd);
+
+    if (UNLIKELY(!timeout_.IsZero() && !cmd_timer_)) {
+			LOG_DEBUG << "InvokeTimer for " << " " << conn()->remote_addr();
+			cmd_timer_ = exec_loop_->RunAfter(timeout_, std::bind(&MemcacheClient::OnPacketTimeout, shared_from_this(), cmd->id()));
+	}
 }
 
 CommandPtr MemcacheClient::PopRunningCommand() {
-    exec_loop_->AssertInLoopThread();
-
-    if (running_command_.empty()) {
+    if (UNLIKELY(running_command_.empty())) {
         return CommandPtr();
     }
 
     CommandPtr command(running_command_.front());
     running_command_.pop();
-
-    if (!timeout_.IsZero() && running_command_.empty()) {
-        if (cmd_timer_) {
-            cmd_timer_->Cancel();
-            cmd_timer_.reset();
-        }
-    }
-
     return command;
 }
 
-CommandPtr MemcacheClient::pop_waiting_command() {
-    if (waiting_command_.empty()) {
+CommandPtr MemcacheClient::PopWaitingCommand() {
+    if (UNLIKELY(waiting_command_.empty())) {
         return CommandPtr();
     }
-
     CommandPtr command(waiting_command_.front());
     waiting_command_.pop();
-
     return command;
 }
 
 void MemcacheClient::OnResponseData(const evpp::TCPConnPtr& tcp_conn,
                                     evpp::Buffer* buf,
                                     evpp::Timestamp ts) {
-    if (!codec_) {
-        codec_ = new BinaryCodec(this);
-    }
-
-    codec_->OnCodecMessage(tcp_conn, buf, ts);
+	if (codec_) {
+			codec_ = new BinaryCodec(this);
+	}
+	codec_->OnCodecMessage(tcp_conn, buf, ts);
 }
 
 void MemcacheClient::OnPacketTimeout(uint32_t cmd_id) {
+	if (!running_command_.empty()) {
+        CommandPtr cmd(running_command_.front());
+		if (LIKELY(cmd->id() != cmd_id)) {
+			cmd_timer_->Cancel();
+			cmd_timer_ = exec_loop_->RunAfter(timeout_, std::bind(&MemcacheClient::OnPacketTimeout, shared_from_this(), cmd->id()));
+		return;
+		}
+	} else {
+		cmd_timer_->Cancel();
+		cmd_timer_.reset();
+		return;
+	}
+
     LOG_DEBUG << "InvokeTimer triggered for " << cmd_id << " " << conn()->remote_addr();
 
-    // cmd_timer_->Cancel();
-    // cmd_timer_.reset();
     while (!running_command_.empty()) {
         CommandPtr cmd(running_command_.front());
         running_command_.pop();
 
         if (mc_pool_ && cmd->ShouldRetry()) {
-            // mc_pool_->LaunchCommand(cmd);
-            cmd->OnError(ERR_CODE_TIMEOUT);
-            //cmd->set_id(0);
-            //cmd->caller_loop()->RunInLoop(std::bind(&MemcacheClientPool::LaunchCommand, mc_pool_, cmd));
+            cmd->set_id(0);
+            cmd->caller_loop()->RunInLoop(std::bind(&MemcacheClientBase::LaunchCommand, mc_pool_, cmd));
         } else {
             cmd->OnError(ERR_CODE_TIMEOUT);
         }
@@ -99,7 +87,6 @@ void MemcacheClient::OnPacketTimeout(uint32_t cmd_id) {
             break;
         }
     }
-
     LOG_WARN << "OnPacketTimeout post, waiting=" << waiting_command_.size()
              << " running=" << running_command_.size();
 }
