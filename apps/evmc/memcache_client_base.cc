a83713190a35b658fc91aa1172374455001b6fc2
zhangke-s <zhangke-s@360.cn>
2017-02-14 19:18:30 +0800

    fix retry bug

--- apps/evmc/memcache_client_base.cc
+++ apps/evmc/memcache_client_base.cc
@@ -120,9 +120,8 @@ void MemcacheClientBase::OnClientConnection(const evpp::TCPConnPtr& conn, Memcac
 
         while (command = memc_client->PopRunningCommand()) {
 			if (command->ShouldRetry()) {
-  //              LOG_ERROR << "OnClientConnection " << conn->remote_addr() <<" waiting, so retry";
                 command->set_server_id(command->server_id()); 
-                command->caller_loop()->RunInLoop(std::bind(&MemcacheClientBase::LaunchCommand, this, command));
+                memc_client->exec_loop()->RunInLoop(std::bind(&MemcacheClientBase::LaunchCommand, this, command));
             } else {
                 command->OnError(ERR_CODE_NETWORK);
             }
@@ -130,8 +129,8 @@ void MemcacheClientBase::OnClientConnection(const evpp::TCPConnPtr& conn, Memcac
 
         while (command = memc_client->PopWaitingCommand()) {
             if (command->ShouldRetry()) {
-    //            LOG_ERROR << "OnClientConnection " << conn->remote_addr() <<" waiting, so retry";
-                command->caller_loop()->RunInLoop(std::bind(&MemcacheClientBase::LaunchCommand, this, command));
+                command->set_server_id(command->server_id()); 
+                memc_client->exec_loop()->RunInLoop(std::bind(&MemcacheClientBase::LaunchCommand, this, command));
             } else {
                 command->OnError(ERR_CODE_NETWORK);
             }
