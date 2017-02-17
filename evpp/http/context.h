ecacdcac5489e428bbe93ce349a4497481400989
weizili.build9 <zieckey@gmail.com>
2017-02-17 12:25:26 +0800

    Refactor : 1, Move evpp::http::Context's data field to private. 2, Fix evnsq,evmc compile error of vs2015

--- evpp/http/context.h
+++ evpp/http/context.h
@@ -2,6 +2,7 @@
 
 #include "evpp/inner_pre.h"
 #include "evpp/slice.h"
+#include "evpp/timestamp.h"
 
 #include <map>
 
@@ -31,20 +32,44 @@ public:
     // 在HTTP请求的HEADER中查找某个key的值。如果没有找到返回一个空指针。
     const char* FindRequestHeader(const char* key);
 
-    static std::string FindClientIP(const char* uri);
+    const  std::string& uri() const {
+        return uri_;
+    }
+
+    const std::string& remote_ip() const {
+        return remote_ip_;
+    }
+
+    const Slice& body() const {
+        return body_;
+    }
+
+    struct evhttp_request* req() const {
+        return req_;
+    }
 
 public:
+    static std::string FindClientIP(const char* uri);
+
+private:
     // 不带参数的URI, 例如: /status.html
-    std::string uri;
+    std::string uri_;
 
     // 远程客户端IP。如果该HTTP请求是由NGINX转发而来，我们会优先使用URL中的‘clientip’参数.
     // @see NGINX反向代理参考配置: proxy_pass http://127.0.0.1:8080/get/?clientip=$remote_addr;
-    std::string remote_ip;
+    std::string remote_ip_;
 
     // HTTP请求的Body数据
-    Slice body;
+    Slice body_;
 
-    struct evhttp_request* req;
+    // The request received timestamp
+    //Timestamp recved_time_;
+    //Timestamp dispached_time_;
+    //Timestamp process_begin_time_;
+    //Timestamp process_end_time_;
+    //Timestamp send_response_time_;
+
+    struct evhttp_request* req_;
 };
 
 typedef std::shared_ptr<Context> ContextPtr;
