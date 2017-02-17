bd4a2cf2cdc082a357e4e210d2b8edce4e1bb0ea
weizili <zieckey@gmail.com>
2017-02-17 12:47:39 +0800

    Fix some compile warnings

--- evpp/platform_config.h
+++ evpp/platform_config.h
@@ -116,6 +116,7 @@
 #pragma warning( disable: 4244 4251 4355 4715 4800 4996 4005 4819)
 #pragma warning( disable: 4530 ) // C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include\xlocale(341): warning C4530: C++ exception handler used, but unwind semantics are not enabled. Specify /EHsc
 #pragma warning( disable: 4577 ) // C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include\exception(359): warning C4577: 'noexcept' used with no exception handling mode specified; termination on exception is not guaranteed. Specify /EHsc
+#pragma warning( disable: 4503 ) // c:\program files (x86)\microsoft visual studio 14.0\vc\include\functional(357): warning C4503: '__LINE__Var': decorated name length exceeded, name was truncated
 #endif
 
 // get rid of Windows/Linux inconsistencies
