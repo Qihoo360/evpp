dccd2013d89b8ae1561242884db05e43970f9a69
zhangke-s <zhangke-s@360.cn>
2017-02-22 17:04:08 +0800

    fix unsupported return status bug

--- apps/evmc/memcache_client.cc
+++ apps/evmc/memcache_client.cc
@@ -16,15 +16,24 @@ void MemcacheClient::PushRunningCommand(CommandPtr& cmd) {
         return;
     }
 
- //   if (cmd->id() == 0) {
-        cmd->set_id(next_id());
- //   }
+	cmd->set_id(next_id());
     running_command_.emplace(cmd);
 
     if (UNLIKELY(!timeout_.IsZero() && timer_canceled_)) {
 			cmd_timer_ = exec_loop_->RunAfter(timeout_, std::bind(&MemcacheClient::OnPacketTimeout, shared_from_this(), cmd->id()));
 		    timer_canceled_ = false;
 	}
+}
+
+void MemcacheClient::PushWaitingCommand(CommandPtr& cmd) {
+        if (LIKELY(cmd)) {
+			cmd->set_id(next_id());
+            waiting_command_.push(cmd);
+        }
+		if (UNLIKELY(!timeout_.IsZero() && con_timer_canceled_)) {
+			con_cmd_timer_ = exec_loop_->RunAfter(timeout_, std::bind(&MemcacheClient::OnConnectTimeout, shared_from_this(), cmd->id()));
+		    con_timer_canceled_ = false;
+		}
 }
 
 CommandPtr MemcacheClient::PopRunningCommand() {
@@ -54,6 +63,36 @@ void MemcacheClient::OnResponseData(const evpp::TCPConnPtr& tcp_conn,
 	}
 
 	codec_->OnCodecMessage(tcp_conn, buf, ts);
+}
+
+
+void MemcacheClient::OnConnectTimeout(uint32_t cmd_id) {
+	con_timer_canceled_ = true;
+	if (!waiting_command_.empty()) {
+        CommandPtr cmd(waiting_command_.front());
+		if (LIKELY(cmd->id() != cmd_id)) {
+			cmd_timer_bakup_.swap(con_cmd_timer_);
+			con_cmd_timer_ = exec_loop_->RunAfter(timeout_, std::bind(&MemcacheClient::OnConnectTimeout, shared_from_this(), cmd->id()));
+		    con_timer_canceled_ = false;
+			return;
+		}
+	} else {
+		return;
+	}
+
+    LOG_DEBUG << "InvokeTimer triggered for " << cmd_id << " " << conn()->remote_addr();
+
+    while (!waiting_command_.empty()) {
+        CommandPtr cmd(waiting_command_.front());
+        waiting_command_.pop();
+
+        if (mc_pool_ && cmd->ShouldRetry()) {
+			cmd->set_server_id(cmd->server_id());
+            exec_loop()->RunInLoop(std::bind(&MemcacheClientBase::LaunchCommand, mc_pool_, cmd));
+        } else {
+            cmd->OnError(ERR_CODE_TIMEOUT);
+        }
+    }
 }
 
 void MemcacheClient::OnPacketTimeout(uint32_t cmd_id) {
