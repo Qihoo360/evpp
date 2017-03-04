012cef02e77d918edd974057234455af0671ab0d
weizili <zieckey@gmail.com>
2017-03-04 17:09:19 +0800

    Add examples/twisted/pubsubserver

--- examples/echo/tcpecho/winmain-inl.h
+++ examples/echo/tcpecho/winmain-inl.h
@@ -4,18 +4,14 @@ namespace {
 struct OnApp {
     OnApp() {
 #ifdef WIN32
-        // Initialize net work.
-        WSADATA wsaData;
         // Initialize Winsock 2.2
+        WSADATA wsaData;
         int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
         if (err) {
             std::cout << "WSAStartup() failed with error: %d" << err;
         }
-
-        //std::cout << "WSAEWOULDBLOCK=" << WSAEWOULDBLOCK << std::endl;
 #endif
-        //std::cout << "EWOULDBLOCK=" << EWOULDBLOCK << " EAGAIN=" << EAGAIN << std::endl;
     }
     ~OnApp() {
 #ifdef WIN32
