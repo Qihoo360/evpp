93f3530ac602522b9b5e4c459ffc0f05ce95bc30
weizili.build9 <zieckey@gmail.com>
2017-03-04 17:28:02 +0800

    Add examples/twisted/webserver

---
+++ examples/twisted/webserver/main.cc
@@ -0,0 +1,32 @@
+#include <atomic>
+
+#include <evpp/exp.h>
+#include <evpp/http/http_server.h>
+#include "../../echo/tcpecho/winmain-inl.h"
+
+std::atomic<int> request_count;
+
+void DefaultHandler(evpp::EventLoop* loop,
+                    const evpp::http::ContextPtr& ctx,
+                    const evpp::http::HTTPSendResponseCallback& cb) {
+    ctx->AddResponseHeader("Content-Type", "text/plain");
+    std::ostringstream os;
+    os << "I am request #" << ++request_count << "\n";
+    cb(os.str());
+}
+
+int main(int argc, char* argv[]) {
+    int port = 8080;
+    if (argc == 2) {
+        port = atoi(argv[1]);
+    }
+
+    evpp::http::Server server;
+    server.RegisterDefaultHandler(&DefaultHandler);
+    server.Init(port);
+    server.Start();
+    while (!server.IsStopped()) {
+        usleep(1);
+    }
+    return 0;
+}
