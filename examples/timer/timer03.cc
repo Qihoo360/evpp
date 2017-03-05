41725da7e354acbc6968bb79a824f6fd1168a478
weizili <zieckey@gmail.com>
2017-03-05 21:34:49 +0800

    Add timer examples

---
+++ examples/timer/timer03.cc
@@ -0,0 +1,24 @@
+#include <evpp/exp.h>
+#include <evpp/event_loop.h>
+
+#ifdef _WIN32
+#include "../echo/tcpecho/winmain-inl.h"
+#endif
+
+void Print(evpp::EventLoop* loop, int* count) {
+    if (*count < 3) {
+        std::cout << "Hello, world! count=" << *count << std::endl;
+        ++(*count);
+        loop->RunAfter(evpp::Duration(2.0), std::bind(&Print, loop, count));
+    } else {
+        loop->Stop();
+    }
+}
+
+int main() {
+    evpp::EventLoop loop;
+    int count = 0;
+    loop.RunAfter(evpp::Duration(2.0), std::bind(&Print, &loop, &count));
+    loop.Run();
+    return 0;
+}
