f13956ef1458a6d85692c3befdce122ef14b0ad3
weizili.build9 <zieckey@gmail.com>
2017-03-05 21:34:49 +0800

    Add timer examples

---
+++ examples/timer/timer01.cc
@@ -0,0 +1,17 @@
+#include <evpp/exp.h>
+#include <evpp/event_loop.h>
+
+#ifdef _WIN32
+#include "../echo/tcpecho/winmain-inl.h"
+#endif
+
+void Print() {
+    std::cout << "Hello, world!\n";
+}
+
+int main() {
+    evpp::EventLoop loop;
+    loop.RunAfter(evpp::Duration(5.0), &Print);
+    loop.Run();
+    return 0;
+}
---
+++ examples/timer/timer05.cc
@@ -0,0 +1,36 @@
+#include <evpp/exp.h>
+#include <evpp/event_loop.h>
+
+#ifdef _WIN32
+#include "../echo/tcpecho/winmain-inl.h"
+#endif
+
+class Printer {
+public:
+    Printer(evpp::EventLoop* l) : loop_(l), count_(0) {}
+    void Print1() {
+        std::cout << "Print1 : count=" << count_ << std::endl;
+        ++count_;
+    }
+
+    void Print2() {
+        std::cout << "Print2 : count=" << count_ << std::endl;
+        ++count_;
+    }
+private:
+    evpp::EventLoop* loop_;
+    int count_;
+};
+
+int main() {
+    evpp::EventLoop loop;
+    Printer printer(&loop);
+    loop.RunEvery(evpp::Duration(1.0), std::bind(&Printer::Print1, &printer));
+    loop.RunEvery(evpp::Duration(2.0), std::bind(&Printer::Print2, &printer));
+    auto f = [&loop]() {
+        loop.Stop();
+    };
+    loop.RunAfter(evpp::Duration(10.0), f);
+    loop.Run();
+    return 0;
+}
