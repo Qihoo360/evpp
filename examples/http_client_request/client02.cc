f0c607f10d562d93283b99133ec72f63b16c7edb
weizili <zieckey@gmail.com>
2017-03-05 22:44:48 +0800

    Add http_client_request examples code

---
+++ examples/http_client_request/client02.cc
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <evpp/libevent_headers.h>
+#include <evpp/event_loop_thread.h>
+
+#include <evpp/httpc/request.h>
+#include <evpp/httpc/conn.h>
+#include <evpp/httpc/response.h>
+
+#include "../echo/tcpecho/winmain-inl.h"
+
+static int responsed = 0;
+static void HandleHTTPResponse(const std::shared_ptr<evpp::httpc::Response>& response, evpp::httpc::GetRequest* request) {
+    LOG_INFO << "http_code=" << response->http_code()
+        << " URL=http://" << request->host() << request->uri()
+        << " [" << response->body().ToString() << "]";
+    const char* header = response->FindHeader("Connection");
+    if (header) {
+        LOG_INFO << "HTTP HEADER Connection=" << header;
+    }
+    responsed++;
+    assert(request == response->request());
+    delete request; // The request MUST BE deleted in EventLoop thread.
+}
+
+int main() {
+    evpp::EventLoopThread t;
+    t.Start(true);
+    evpp::httpc::GetRequest* r = new evpp::httpc::GetRequest(t.event_loop(), "http://www.360.cn/robots.txt", evpp::Duration(5.0));
+    r->Execute(std::bind(&HandleHTTPResponse, std::placeholders::_1, r));
+    r = new evpp::httpc::GetRequest(t.event_loop(), "http://www.sohu.com/robots.txt", evpp::Duration(5.0));
+    r->Execute(std::bind(&HandleHTTPResponse, std::placeholders::_1, r));
+    while (responsed != 2) {
+        usleep(1);
+    }
+
+    t.Stop(true);
+    LOG_INFO << "EventLoopThread stopped.";
+    return 0;
+}
---
+++ examples/http_client_request/client04.cc
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <evpp/event_loop_thread.h>
+
+#include <evpp/httpc/conn_pool.h>
+#include <evpp/httpc/request.h>
+#include <evpp/httpc/conn.h>
+#include <evpp/httpc/response.h>
+
+#include "../echo/tcpecho/winmain-inl.h"
+
+static int responsed = 0;
+static int retried = 0;
+static void HandleHTTPResponse(const std::shared_ptr<evpp::httpc::Response>& r, evpp::httpc::Request* req) {
+    LOG_INFO << "http_code=" << r->http_code() << " [" << r->body().ToString() << "]";
+    responsed++;
+    if (retried < 3) {
+        retried++;
+        req->Execute(std::bind(&HandleHTTPResponse, std::placeholders::_1, req));
+    } else {
+        delete req;
+    }
+}
+
+int main() {
+    evpp::EventLoopThread t;
+    t.Start(true);
+    evpp::httpc::Request* r = new evpp::httpc::Request(t.event_loop(), "http://www.360.cn/robots.txt", "", evpp::Duration(2.0));
+    LOG_INFO << "Do http request";
+    r->Execute(std::bind(&HandleHTTPResponse, std::placeholders::_1, r));
+
+    while (responsed != 4) {
+        usleep(1);
+    }
+
+    t.Stop(true);
+    LOG_INFO << "EventLoopThread stopped.";
+    return 0;
+}
