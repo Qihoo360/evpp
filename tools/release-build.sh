36156d80d0219af0fa86bb33022ec848f6ba796f
weizili <zieckey@gmail.com>
2017-03-06 13:02:35 +0800

    Move build script files to tools

--- tools/release-build.sh
+++ tools/release-build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# Run this script at the directory which holds it.
+
 set -x
   
-SOURCE_DIR=`pwd`
+SOURCE_DIR=`pwd`/..
 BUILD_TYPE=release
-BUILD_DIR=build-${BUILD_TYPE}
+BUILD_DIR=${SOURCE_DIR}/build-${BUILD_TYPE}
 
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR \
