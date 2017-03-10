f9d8dda7a73d52522fb43c819debc3a4ab81c3de
weizili <zieckey@gmail.com>
2017-03-10 14:37:28 +0800

    Update benchmark script

--- benchmark/tcp/libevent/single_thread.sh
+++ benchmark/tcp/libevent/single_thread.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-killall server
+killall benchmark_tcp_libevent_server
 timeout=${timeout:-100}
 bufsize=${bufsize:-16384}
 nothreads=1
@@ -8,9 +8,9 @@ nothreads=1
 for nosessions in 1 10 100 1000 10000; do
   sleep 5
   echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
-  taskset -c 1 ./server 2> /dev/null & srvpid=$!
+  taskset -c 1 ./benchmark_tcp_libevent_server 2> /dev/null & srvpid=$!
   sleep 1
-  taskset -c 2 ./client 9876 $bufsize $nosessions $timeout
+  taskset -c 2 ./benchmark_tcp_libevent_client 9876 $bufsize $nosessions $timeout
   kill -9 $srvpid
 done
 
