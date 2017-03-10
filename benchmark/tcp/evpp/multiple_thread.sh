f8ad4a2c24a743043920edfe56ead493f2dc9de8
weizili <zieckey@gmail.com>
2017-03-10 13:28:37 +0800

    Add benchmark codes

---
+++ benchmark/tcp/evpp/multiple_thread.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+killall example_pingpong_server
+timeout=${timeout:-100}
+bufsize=${bufsize:-16384}
+
+for nosessions in 100 1000; do
+  for nothreads in 1 2 3 4; do
+    sleep 5
+    echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
+    ./example_pingpong_server 33333 $nothreads & srvpid=$!
+    sleep 1
+    ./example_pingpong_client 127.0.0.1 33333 $nothreads $bufsize $nosessions $timeout
+    kill -9 $srvpid
+  done
+done
