60767bab72a252f72e52312c40a2aff7388b8c99
weizili <zieckey@gmail.com>
2017-03-10 14:37:28 +0800

    Update benchmark script

--- benchmark/tcp/asio/build.sh
+++ benchmark/tcp/asio/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-g++ -O2 -finline-limit=1000 server.cpp -o server -lpthread -lboost_thread -lboost_system
-g++ -O2 -finline-limit=1000 client.cpp -o client -lpthread -lboost_thread -lboost_system
-# g++ -O2 -finline-limit=1000 -I../asio-1.4.5/include server.cpp -o server145 -lpthread
-# g++ -O2 -finline-limit=1000 -I../asio-1.4.5/include client.cpp -o client145 -lpthread
+g++ -O2 -finline-limit=1000 server.cpp -o benchmark_tcp_asio_server -lpthread -lboost_thread -lboost_system
+g++ -O2 -finline-limit=1000 client.cpp -o benchmark_tcp_asio_client -lpthread -lboost_thread -lboost_system
