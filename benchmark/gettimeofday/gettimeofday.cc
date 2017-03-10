77a2ee25e0cef1ab41be7750aa41f28ace7d9914
weizili <zieckey@gmail.com>
2017-03-10 20:23:07 +0800

    Add steady_clock_benchmark

--- benchmark/gettimeofday/gettimeofday.cc
+++ benchmark/gettimeofday/gettimeofday.cc
@@ -32,6 +32,16 @@ uint64_t high_resolution_clock_benchmark(int loop) {
     return rc;
 }
 
+
+uint64_t steady_clock_benchmark(int loop) {
+    uint64_t rc = 0;
+    for (int i = 0; i < loop; ++i) {
+        auto ts = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+        rc += ts;
+    }
+    return rc;
+}
+
 typedef std::function<uint64_t(int)> BenchmarkFunctor;
 void Benchmark(BenchmarkFunctor f, const std::string& name) {
     int loop = 1000 * 1000 * 10;
@@ -53,6 +63,7 @@ high_resolution_clock_benchmark loop=10000000 cost=1.25619s op=125.619ns/op QPS=
 int main() {
     Benchmark(&gettimeofday_benchmark,          "         gettimeofday_benchmark");
     Benchmark(&system_clock_benchmark,          "         system_clock_benchmark");
+    Benchmark(&steady_clock_benchmark,          "         steady_clock_benchmark");
     Benchmark(&high_resolution_clock_benchmark, "high_resolution_clock_benchmark");
     return 0;
 }
