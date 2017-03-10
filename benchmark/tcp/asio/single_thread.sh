3ec6e5c8c176545b1cb948782678f1787ac7e413
weizili <zieckey@gmail.com>
2017-03-10 14:37:28 +0800

    Update benchmark script

--- benchmark/tcp/asio/single_thread.sh
+++ benchmark/tcp/asio/single_thread.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-killall server
+killall benchmark_tcp_asio_server
 timeout=${timeout:-100}
 bufsize=${bufsize:-16384}
 nothreads=1
@@ -8,9 +8,9 @@ nothreads=1
 for nosessions in 1 10 100 1000 10000; do
   sleep 5
   echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
-  taskset -c 1 ./server 0.0.0.0 55555 $nothreads $bufsize & srvpid=$!
+  taskset -c 1 ./benchmark_tcp_asio_server 0.0.0.0 55555 $nothreads $bufsize & srvpid=$!
   sleep 1
-  taskset -c 2 ./client 127.0.0.1 55555 $nothreads $bufsize $nosessions $timeout
+  taskset -c 2 ./benchmark_tcp_asio_client 127.0.0.1 55555 $nothreads $bufsize $nosessions $timeout
   kill -9 $srvpid
 done
 
