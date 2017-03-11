2341956e67b6b731d3a9be6ed4acd41128e87c96
weizili <zieckey@gmail.com>
2017-03-11 18:30:13 +0800

    Add comments

--- tools/release-build.sh
+++ tools/release-build.sh
@@ -2,6 +2,14 @@
 
 # Run this script at the directory which holds it.
 
+#Try to checkout to the lastest TAG and compile it
+#
+#VERSION=`grep PACKAGE_VERSION ../cmake/packages.cmake  | head -1 | awk -F\" '{print $2}' | awk -F"$" '{print $1}'`
+#VERSION=`echo -n ${VERSION}`
+#
+# git checkout $VERSION
+#
+
 set -x
   
 SOURCE_DIR=`pwd`/..
