21ab689f92ec13afdd6cd6ed0188a73182cecf72
weizili <zieckey@gmail.com>
2017-03-11 09:55:07 +0800

    Add tcp_server and tcp_client examples

---
+++ examples/tcp/tcp_client.cc
@@ -0,0 +1,35 @@
+#include <evpp/tcp_client.h>
+#include <evpp/buffer.h>
+#include <evpp/tcp_conn.h>
+
+int main(int argc, char* argv[]) {
+    std::string addr = "127.0.0.1:9099";
+
+    if (argc == 2) {
+        addr = argv[1];
+    }
+
+    evpp::EventLoop loop;
+    evpp::TCPClient client(&loop, addr, "TCPPingPongClient");
+    client.SetMessageCallback([&loop, &client](const evpp::TCPConnPtr& conn,
+                               evpp::Buffer* msg,
+                               evpp::Timestamp ts) {
+        LOG_TRACE << "Receive a message [" << msg->ToString() << "]";
+        client.Disconnect();
+        loop.RunAfter(500.0, [&loop]() { loop.Stop(); });
+    });
+
+    client.SetConnectionCallback([](const evpp::TCPConnPtr& conn) {
+        if (conn->IsConnected()) {
+            LOG_INFO << "Connected to " << conn->remote_addr();
+            conn->Send("hello");
+        } else {
+            LOG_INFO << "Disconnected from " << conn->remote_addr();
+        }
+    });
+    client.Connect();
+    loop.Run();
+    return 0;
+}
+
+#include "../echo/tcpecho/winmain-inl.h"
---
+++ examples/tcp/tcp_server.cc
@@ -0,0 +1,29 @@
+#include <evpp/tcp_server.h>
+#include <evpp/buffer.h>
+#include <evpp/tcp_conn.h>
+
+int main(int argc, char* argv[]) {
+    std::string addr = "0.0.0.0:9099";
+    int thread_num = 4;
+    evpp::EventLoop loop;
+    evpp::TCPServer server(&loop, addr, "TCPEchoServer", thread_num);
+    server.SetMessageCallback([](const evpp::TCPConnPtr& conn,
+                                 evpp::Buffer* msg,
+                                 evpp::Timestamp ts) {
+        conn->Send(msg);
+    });
+    server.SetConnectionCallback([](const evpp::TCPConnPtr& conn) {
+        if (conn->IsConnected()) {
+            LOG_INFO << "A new connection from " << conn->remote_addr();
+        } else {
+            LOG_INFO << "Lost the connection from " << conn->remote_addr();
+        }
+    });
+    server.Init();
+    server.Start();
+    loop.Run();
+    return 0;
+}
+
+#include "../echo/tcpecho/winmain-inl.h"
+
