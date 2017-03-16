841cd233eaa3e9ff521353f9cea2883e72cf51e2
weizili <zieckey@gmail.com>
2017-03-16 11:05:05 +0800

    Add asio from huyuguang test script

--- benchmark/throughput/asio/single_thread.sh
+++ benchmark/throughput/asio/single_thread.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 
-killall benchmark_tcp_asio_server
-timeout=${timeout:-100}
-bufsize=${bufsize:-16384}
+#set -x
+
+killall asio_test.exe
+timeout=${timeout:-10}
+#bufsize=${bufsize:-16384}
 nothreads=1
 
+for bufsize in 1024 2048 4096 8192 16384 81920; do
 for nosessions in 1 10 100 1000 10000; do
-  sleep 5
-  echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
-  taskset -c 1 ./benchmark_tcp_asio_server 0.0.0.0 55555 $nothreads $bufsize & srvpid=$!
+  echo "======================> (test1) Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
   sleep 1
-  taskset -c 2 ./benchmark_tcp_asio_client 127.0.0.1 55555 $nothreads $bufsize $nosessions $timeout
+  taskset -c 1 ./asio_test.exe server1 127.0.0.1 33333 $nothreads $bufsize & srvpid=$!
+  sleep 1
+  taskset -c 2 ./asio_test.exe client1 127.0.0.1 33333 $nothreads $bufsize $nosessions $timeout
+  sleep 1
   kill -9 $srvpid
+  sleep 5
+done
 done
 
+for bufsize in 1024 2048 4096 8192 16384 81920; do
+for nosessions in 1 10 100 1000 10000; do
+  echo "======================> (test2) Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
+  sleep 1
+  taskset -c 1 ./asio_test.exe server2 127.0.0.1 33333 $nothreads $bufsize & srvpid=$!
+  sleep 1
+  taskset -c 2 ./asio_test.exe client2 127.0.0.1 33333 $nothreads $bufsize $nosessions $timeout
+  sleep 1
+  kill -9 $srvpid
+  sleep 5
+done
+done
