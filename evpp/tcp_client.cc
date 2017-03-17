8a77551c3c3e56173d25ce11a013d600e1920e80
weizili <zieckey@gmail.com>
2017-03-17 17:33:02 +0800

    Fix a bug : crash when TCPClient connect failed on DEBUG mode. See https://github.com/Qihoo360/evpp/issues/7

--- evpp/tcp_client.cc
+++ evpp/tcp_client.cc
@@ -22,7 +22,9 @@ TCPClient::~TCPClient() {
     assert(!connector_.get());
     auto_reconnect_.store(false);
     TCPConnPtr c = conn();
-    assert(c->IsDisconnected());
+    if (c) {
+        assert(c->IsDisconnected());
+    }
     conn_.reset();
 }
 
