eadd021fe024a99bd56faa2fbc907561a7e9ad08
weizili <zieckey@gmail.com>
2017-03-21 18:27:40 +0800

    Add evnsq::Client::IsReady

--- apps/evnsq/producer.h
+++ apps/evnsq/producer.h
@@ -21,16 +21,28 @@ public:
     bool Publish(const std::string& topic, const std::string& msg);
     bool MultiPublish(const std::string& topic, const std::vector<std::string>& messages);
 
-    // A PUB/MPUB command which has already serialized.
+    // A PUB/MPUB command which has already been serialized.
     bool PublishBinaryCommand(evpp::Buffer* command_binary_buf);
 
     void SetReadyCallback(const ReadyCallback& cb) {
         ready_fn_ = cb;
     }
     void SetHighWaterMarkCallback(const HighWaterMarkCallback& cb, size_t mark);
+
+public:
+    size_t published_count() const {
+        return published_count_;
+    }
+    size_t published_ok_count() const {
+        return published_ok_count_;
+    }
+    size_t published_failed_count() const {
+        return published_failed_count_;
+    }
     size_t high_water_mark() const {
         return high_water_mark_;
     }
+
 private:
     bool Publish(const CommandPtr& cmd);
     bool PublishInLoop(const CommandPtr& cmd);
@@ -45,10 +57,11 @@ private:
     size_t published_ok_count_;
     size_t published_failed_count_;
 
+    // TODO add HWM relative logic code
     enum { kDefaultHighWaterMark = 1024 };
     bool hwm_triggered_; // The flag of high water mark
     HighWaterMarkCallback high_water_mark_fn_;
-    size_t high_water_mark_; // The default value is kDefaultHighWaterMark
+    size_t high_water_mark_; // The high water mark for message count. default value is kDefaultHighWaterMark
 };
 
 
