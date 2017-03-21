f76e35e0b906d7ae7d4b6da650986825bc878a03
weizili <zieckey@gmail.com>
2017-03-21 21:10:22 +0800

    Fix EventWatcher::FreeEvent attached_ do not be setted to false. Add InvokeTimer::set_cancel_callback. Use lambda to replace InvokeTimer::AsyncWait. Use loop_->QueueInLoop to run InvokeTimer::Cancel()

--- evpp/invoke_timer.h
+++ evpp/invoke_timer.h
@@ -20,16 +20,19 @@ public:
     ~InvokeTimer();
     void Start();
     void Cancel();
+    void set_cancel_callback(const Functor& fn) {
+        cancel_callback_ = fn;
+    }
 private:
     InvokeTimer(EventLoop* evloop, Duration timeout, const Functor& f, bool periodic);
     InvokeTimer(EventLoop* evloop, Duration timeout, Functor&& f, bool periodic);
-    void AsyncWait();
     void OnTimerTriggered();
     void OnCanceled();
 private:
     EventLoop* loop_;
     Duration timeout_;
     Functor functor_;
+    Functor cancel_callback_;
     std::shared_ptr<TimerEventWatcher> timer_;
     bool periodic_;
     std::shared_ptr<InvokeTimer> self_; // Hold myself
