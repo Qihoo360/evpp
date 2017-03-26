dc55f06c6e377fcbca1bc9bbbabb7898e43533d5
weizili <zieckey@gmail.com>
2017-03-26 20:23:06 +0800

    Add more signal examples

---
+++ examples/signal/signal01.cc
@@ -0,0 +1,18 @@
+#include <signal.h>
+#include <evpp/exp.h>
+#include <evpp/libevent_watcher.h>
+#include <evpp/event_loop.h>
+
+#include "examples/winmain-inl.h"
+
+int main(int argc, char* argv[]) {
+    evpp::EventLoop loop;
+    std::unique_ptr<evpp::SignalEventWatcher> ev(
+        new evpp::SignalEventWatcher(
+            SIGINT, &loop, []() { LOG_INFO << "SIGINT caught.";}));
+    ev->Init();
+    ev->AsyncWait();
+    loop.Run();
+    return 0;
+}
+
