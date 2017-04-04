65f488a569b412bdbad3e3a7fe06be616cc42e18
weizili <zieckey@gmail.com>
2017-04-04 15:51:05 +0800

    Add simple chatroom

---
+++ examples/chatroom/simple/client.cc
@@ -0,0 +1,84 @@
+#include "codec.h"
+
+#include <evpp/event_loop.h>
+#include <evpp/event_loop_thread.h>
+#include <evpp/tcp_client.h>
+
+#include <mutex>
+#include <iostream>
+#include <stdio.h>
+
+class ChatClient {
+public:
+    ChatClient(evpp::EventLoop* loop, const std::string& serverAddr)
+        : client_(loop, serverAddr, "ChatClient"),
+        codec_(std::bind(&ChatClient::OnStringMessage, this, std::placeholders::_1, std::placeholders::_2)) {
+        client_.SetConnectionCallback(
+            std::bind(&ChatClient::OnConnection, this, std::placeholders::_1));
+        client_.SetMessageCallback(
+            std::bind(&LengthHeaderCodec::OnMessage, &codec_, std::placeholders::_1, std::placeholders::_2));
+    }
+
+    void Connect() {
+        client_.Connect();
+    }
+
+    void Disconnect() {
+        client_.Disconnect();
+    }
+
+    void Write(const evpp::Slice& message) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (connection_) {
+            codec_.Send(connection_, message);
+        }
+    }
+
+private:
+    void OnConnection(const evpp::TCPConnPtr& conn) {
+        LOG_INFO << conn->AddrToString() << " is " << (conn->IsConnected() ? "UP" : "DOWN");
+
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (conn->IsConnected()) {
+            connection_ = conn;
+        } else {
+            connection_.reset();
+        }
+    }
+
+    void OnStringMessage(const evpp::TCPConnPtr&,
+                         const std::string& message) {
+        fprintf(stdout, "<<< %s\n", message.c_str());
+        fflush(stdout);
+    }
+
+    evpp::TCPClient client_;
+    LengthHeaderCodec codec_;
+    std::mutex mutex_;
+    evpp::TCPConnPtr connection_;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s host_ip port\n", argv[0]);
+        return -1;
+    }
+    evpp::EventLoopThread loop;
+    loop.Start(true);
+    std::string host = argv[1];
+    std::string port = argv[2];
+
+    ChatClient client(loop.event_loop(), host + ":" + port);
+    client.Connect();
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (line == "quit") {
+            client.Disconnect();
+            break;
+        }
+        client.Write(line);
+    }
+    loop.Stop(true);
+    return 0;
+}
+
---
+++ examples/chatroom/simple/readme.md
@@ -0,0 +1,24 @@
+A simple chatroom
+
+The original example code is at here [http://www.boost.org/doc/libs/1_35_0/doc/html/boost_asio/example/chat/chat_server.cpp](http://www.boost.org/doc/libs/1_35_0/doc/html/boost_asio/example/chat/chat_server.cpp).
+
+And the implementation of [evpp]'s simple chatroom is modified from [muduo]'s examples [https://github.com/chenshuo/muduo/tree/master/examples/asio/chat](https://github.com/chenshuo/muduo/tree/master/examples/asio/chat).
+
+
+
+[gtest]:https://github.com/google/googletest
+[glog]:https://github.com/google/glog
+[Golang]:https://golang.org
+[muduo]:https://github.com/chenshuo/muduo
+[libevent]:https://github.com/libevent/libevent
+[libevent2]:https://github.com/libevent/libevent
+[LevelDB]:https://github.com/google/leveldb
+[rapidjson]:https://github.com/miloyip/
+[Boost.Asio]:http://www.boost.org/
+[boost.asio]:http://www.boost.org/
+[asio]:http://www.boost.org/
+[boost]:http://www.boost.org/
+[evpp]:https://github.com/Qihoo360/evpp
+[evmc]:https://github.com/Qihoo360/evpp/tree/master/apps/evmc
+[evnsq]:https://github.com/Qihoo360/evpp/tree/master/apps/evnsq
+
