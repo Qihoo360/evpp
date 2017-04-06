1ac5240404503b7f2c4f924ef8ca4c87b4748b07
weizili <zieckey@gmail.com>
2017-04-06 15:19:40 +0800

    Fix compile warnings of "concurrentqueue/concurrentqueue.h" -Wshadow. Fix libevpp_concurrentqueue.so version problem

--- tools/debug-build.sh
+++ tools/debug-build.sh
@@ -11,4 +11,5 @@ BUILD_DIR=${SOURCE_DIR}/build-${BUILD_TYPE}
 mkdir -p $BUILD_DIR \
   && cd $BUILD_DIR \
   && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE $SOURCE_DIR \
-  && make -j
+  && make -j \
+  && make package
