61bd577c6485318c2d0301ecf455f7d0bc246d53
weizili <zieckey@gmail.com>
2017-04-13 20:03:35 +0800

    Add recipes/self_control_timer

---
+++ examples/recipes/self_control_timer/basic_02/main.cc
@@ -0,0 +1,17 @@
+#include "invoke_timer.h"
+#include "event_watcher.h"
+#include "winmain-inl.h"
+
+#include <event2/event.h>
+
+void Print() {
+    std::cout << __FUNCTION__ << " hello world." << std::endl;
+}
+
+int main() {
+    struct event_base* base = event_base_new();
+    auto timer = recipes::InvokeTimer(base, 1000.0, &Print);
+    timer.Start();
+    event_base_dispatch(base);
+    return 0;
+}
---
+++ examples/recipes/self_control_timer/basic_periodic_03/main.cc
@@ -0,0 +1,18 @@
+#include "invoke_timer.h"
+#include "event_watcher.h"
+#include "winmain-inl.h"
+
+#include <event2/event.h>
+
+void Print() {
+    std::cout << __FUNCTION__ << " hello world." << std::endl;
+}
+
+int main() {
+    struct event_base* base = event_base_new();
+    auto timer = recipes::InvokeTimer::Create(base, 1000.0, &Print, false);
+    timer->Start();
+    timer.reset();
+    event_base_dispatch(base);
+    return 0;
+}
