31a73e316b6b83a9303490f961309221f4b0fc4c
weizili <zieckey@gmail.com>
2017-04-14 10:22:44 +0800

    Fix a bug : DNSResolver will invoke callback twice when it is timeout See https://github.com/Qihoo360/evpp/issues/32

--- test/dns_resolver_test.cc
+++ test/dns_resolver_test.cc
@@ -16,7 +16,8 @@ TEST_UNIT(testDNSResolver) {
         evpp::Duration delay(double(3.0)); // 3s
         std::unique_ptr<evpp::EventLoopThread> t(new evpp::EventLoopThread);
         t->Start(true);
-        std::shared_ptr<evpp::DNSResolver> dns_resolver(new evpp::DNSResolver(t->loop(), "www.so.com", evpp::Duration(1.0), fn_resolved));
+        std::shared_ptr<evpp::DNSResolver> dns_resolver(
+            new evpp::DNSResolver(t->loop(), "www.so.com", delay, fn_resolved));
         dns_resolver->Start();
 
         while (!resolved) {
@@ -36,6 +37,53 @@ TEST_UNIT(testDNSResolver) {
 
         t->Stop(true);
         t.reset();
+        if (evpp::GetActiveEventCount() != 0) {
+            H_TEST_ASSERT(evpp::GetActiveEventCount() == 0);
+        }
+    }
+}
+
+TEST_UNIT(testDNSResolverTimeout) {
+    for (int i = 0; i < 6; i++) {
+        std::atomic<int> resolved(0);
+        bool deleted = false;
+        auto fn_resolved = [&resolved](const std::vector <struct in_addr>& addrs) {
+            LOG_INFO << "Entering fn_resolved addrs.size=" << addrs.size();
+            resolved.fetch_add(1);
+        };
+
+        // 1us to make it timeout immediately
+        evpp::Duration delay(double(0.000001));
+        std::unique_ptr<evpp::EventLoopThread> t(new evpp::EventLoopThread);
+        t->Start(true);
+        std::shared_ptr<evpp::DNSResolver> dns_resolver(
+            new evpp::DNSResolver(t->loop(),
+                                  "wwwwwww.en.cppreference.com",
+                                  delay,
+                                  fn_resolved));
+        dns_resolver->Start();
+
+        while (!resolved) {
+            usleep(1);
+        }
+
+        auto fn_deleter = [&deleted, dns_resolver]() {
+            LOG_INFO << "Entering fn_deleter";
+            deleted = true;
+        };
+
+        t->loop()->QueueInLoop(fn_deleter);
+        dns_resolver.reset();
+        while (!deleted) {
+            usleep(1);
+        }
+
+        t->loop()->RunAfter(evpp::Duration(0.05), [loop = t.get()]() { loop->Stop(); });
+        while (!t->IsStopped()) {
+            usleep(1);
+        }
+        t.reset();
+        assert(resolved.load() == 1);
         if (evpp::GetActiveEventCount() != 0) {
             H_TEST_ASSERT(evpp::GetActiveEventCount() == 0);
         }
