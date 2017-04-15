7055effe943d281a2daa53770cea265b7fe2911e
weizili <zieckey@gmail.com>
2017-04-15 21:28:16 +0800

    Add cancel_03 code. Update reame.md

--- examples/recipes/self_control_timer/basic_01/invoke_timer.h
+++ examples/recipes/self_control_timer/basic_01/invoke_timer.h
@@ -14,16 +14,9 @@ class InvokeTimer {
 public:
     typedef std::function<void()> Functor;
 
-    // @brief Create a timer. When the timer is timeout, the functor f will
-    //  be invoked automatically.
-    // @param evloop - The EventLoop runs this timer
-    // @param timeout - The timeout when the timer is invoked
-    // @param f -
     InvokeTimer(struct event_base* evloop, double timeout_ms, const Functor& f);
     ~InvokeTimer();
 
-    // It is thread safe.
-    // Start this timer.
     void Start();
 
 private:
