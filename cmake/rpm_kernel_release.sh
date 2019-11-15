f89875a02e3a7143fe3b83f1b1d4be231327c8ea
wuhan1 <wuhan1@360.cn>
2019-11-15 11:10:29 +0800

    Fix docker have no kernel

--- cmake/rpm_kernel_release.sh
+++ cmake/rpm_kernel_release.sh
@@ -1,1 +1,8 @@
-echo -n $(uname -r|awk -F"." '{print $(NF-1) "." $NF}')
+#Docker has no kernel
+#Check if evpp is in container
+INCONTAINE=`cat /proc/1/cgroup  | grep 'docker\|lxc'`
+if [ ! -z "$INCONTAINE" ]; then
+    echo -n $(cat /etc/redhat-release | awk -F ' ' '{printf("%s.%s.%s\n",$1,$2,$3)}')
+else
+    echo -n $(uname -r | awk -F"." '{print $(NF-1) "." $NF}')
+fi
